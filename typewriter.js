897ad9b3a0d1f6957bc7b5be030b9508877f2079
Luisa Bolado <mboladop@hotmail.com>
2018-07-28 18:24:20 +0000

    typewriter function

--- typewriter.js
+++ typewriter.js
@@ -7,6 +7,12 @@ function typeWriter(text, i) {
             }, 200);
         }
     }
-    if ($('.jumbotron.j2') == true) {
-        typeWriter("It´s not a bug! It´s a Feature!", 0);
-    }
+    var notStarted = true;
+    $(window).scroll(function() {
+	var $height = $(window).scrollTop();
+  if($height > 645 && notStarted) {
+    notStarted = false; 
+	typeWriter("It´s not a bug! It´s a Feature!", 0);
+	}
+});
+        
